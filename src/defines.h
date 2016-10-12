968bd9b9750a6b330db9db4dea8f4895ce7607ee
TomT <tom.triboulot@efrei.net>
2016-10-12 15:06:02 +0200

    Remplacement taille provisoire des cases et suppression de la variable is_aerial

--- src/defines.h
+++ src/defines.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 #define NB_BLOCKS   8
-#define BLOCK_SIZE  64
+#define BLOCK_SIZE  80
 #define BOARD_SIZE  NB_BLOCKS * BLOCK_SIZE
 
 typedef struct units
@@ -13,7 +13,6 @@ typedef struct units
     short ID;
     short range;
     short blocks_per_move;
-    short is_aerial;
 }units;
 
 enum units_ID { rabble, spearman, crossbowman, l_horse, h_horse, trebuchet, catapult, elephant, dragon, king };
